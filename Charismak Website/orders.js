6a1e8f70d6c2bfafe9b52ed0dcc057676eac07a9
charismak <100615875+charismak@users.noreply.github.com>
2022-04-09 11:07:56 +0100

    Add files via upload
    CHARISMAK DASHBOARD

---
+++ Charismak Website/orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Concrete',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Concrete',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Concrete',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Concrete',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Concrete',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+]
